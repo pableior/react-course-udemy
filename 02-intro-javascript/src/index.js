ee279f1e8efc73e8da558546127a6ef17ce8e8c7
Pablo Gonzalez <pableior@gmail.com>
2024-01-03 11:14:59 -0300

    first commit

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,16 @@
+
+const firstName = 'Pablo';
+const lastName = 'Gonzalez';
+let diceValue = 5;
+
+diceValue = 6;
+console.log(firstName, lastName, diceValue);
+
+if (true) {
+    const firstName = 'Peter';
+    let diceValue = 1;
+    console.log(firstName, diceValue);
+}
+
+console.log(firstName, diceValue);
+
